2a521b0cdd4bca3e64f980180143249bc023295a
jemoralesuvg <49993116+jemoralesuvg@users.noreply.github.com>
2022-11-15 10:57:59 -0600

    pruebas de tivaware

---
+++ Tivaware/Input/Debug/main_input.d
@@ -0,0 +1,51 @@
+# FIXED
+
+main_input.obj: ../main_input.c
+main_input.obj: /home/kurt/bin/code_composer/ccs1210/ccs/tools/compiler/ti-cgt-arm_20.2.7.LTS/include/stdint.h
+main_input.obj: /home/kurt/bin/code_composer/ccs1210/ccs/tools/compiler/ti-cgt-arm_20.2.7.LTS/include/_ti_config.h
+main_input.obj: /home/kurt/bin/code_composer/ccs1210/ccs/tools/compiler/ti-cgt-arm_20.2.7.LTS/include/linkage.h
+main_input.obj: /home/kurt/bin/code_composer/ccs1210/ccs/tools/compiler/ti-cgt-arm_20.2.7.LTS/include/_stdint40.h
+main_input.obj: /home/kurt/bin/code_composer/ccs1210/ccs/tools/compiler/ti-cgt-arm_20.2.7.LTS/include/sys/stdint.h
+main_input.obj: /home/kurt/bin/code_composer/ccs1210/ccs/tools/compiler/ti-cgt-arm_20.2.7.LTS/include/sys/cdefs.h
+main_input.obj: /home/kurt/bin/code_composer/ccs1210/ccs/tools/compiler/ti-cgt-arm_20.2.7.LTS/include/sys/_types.h
+main_input.obj: /home/kurt/bin/code_composer/ccs1210/ccs/tools/compiler/ti-cgt-arm_20.2.7.LTS/include/machine/_types.h
+main_input.obj: /home/kurt/bin/code_composer/ccs1210/ccs/tools/compiler/ti-cgt-arm_20.2.7.LTS/include/machine/_stdint.h
+main_input.obj: /home/kurt/bin/code_composer/ccs1210/ccs/tools/compiler/ti-cgt-arm_20.2.7.LTS/include/sys/_stdint.h
+main_input.obj: /home/kurt/bin/code_composer/ccs1210/ccs/tools/compiler/ti-cgt-arm_20.2.7.LTS/include/stdbool.h
+main_input.obj: /home/kurt/bin/code_composer/install_files/tivaware-2.2.0.295/inc/hw_memmap.h
+main_input.obj: /home/kurt/bin/code_composer/install_files/tivaware-2.2.0.295/driverlib/debug.h
+main_input.obj: /home/kurt/bin/code_composer/install_files/tivaware-2.2.0.295/driverlib/gpio.h
+main_input.obj: /home/kurt/bin/code_composer/install_files/tivaware-2.2.0.295/driverlib/sysctl.h
+
+../main_input.c:
+
+/home/kurt/bin/code_composer/ccs1210/ccs/tools/compiler/ti-cgt-arm_20.2.7.LTS/include/stdint.h:
+
+/home/kurt/bin/code_composer/ccs1210/ccs/tools/compiler/ti-cgt-arm_20.2.7.LTS/include/_ti_config.h:
+
+/home/kurt/bin/code_composer/ccs1210/ccs/tools/compiler/ti-cgt-arm_20.2.7.LTS/include/linkage.h:
+
+/home/kurt/bin/code_composer/ccs1210/ccs/tools/compiler/ti-cgt-arm_20.2.7.LTS/include/_stdint40.h:
+
+/home/kurt/bin/code_composer/ccs1210/ccs/tools/compiler/ti-cgt-arm_20.2.7.LTS/include/sys/stdint.h:
+
+/home/kurt/bin/code_composer/ccs1210/ccs/tools/compiler/ti-cgt-arm_20.2.7.LTS/include/sys/cdefs.h:
+
+/home/kurt/bin/code_composer/ccs1210/ccs/tools/compiler/ti-cgt-arm_20.2.7.LTS/include/sys/_types.h:
+
+/home/kurt/bin/code_composer/ccs1210/ccs/tools/compiler/ti-cgt-arm_20.2.7.LTS/include/machine/_types.h:
+
+/home/kurt/bin/code_composer/ccs1210/ccs/tools/compiler/ti-cgt-arm_20.2.7.LTS/include/machine/_stdint.h:
+
+/home/kurt/bin/code_composer/ccs1210/ccs/tools/compiler/ti-cgt-arm_20.2.7.LTS/include/sys/_stdint.h:
+
+/home/kurt/bin/code_composer/ccs1210/ccs/tools/compiler/ti-cgt-arm_20.2.7.LTS/include/stdbool.h:
+
+/home/kurt/bin/code_composer/install_files/tivaware-2.2.0.295/inc/hw_memmap.h:
+
+/home/kurt/bin/code_composer/install_files/tivaware-2.2.0.295/driverlib/debug.h:
+
+/home/kurt/bin/code_composer/install_files/tivaware-2.2.0.295/driverlib/gpio.h:
+
+/home/kurt/bin/code_composer/install_files/tivaware-2.2.0.295/driverlib/sysctl.h:
+
---
+++ Tivaware/Input/Debug/subdir_vars.mk
@@ -0,0 +1,36 @@
+################################################################################
+# Automatically-generated file. Do not edit!
+################################################################################
+
+# Add inputs and outputs from these tool invocations to the build variables 
+CMD_SRCS += \
+../tm4c1233h6pm.cmd 
+
+LIB_SRCS += \
+/home/kurt/bin/code_composer/install_files/tivaware-2.2.0.295/driverlib/ccs/Debug/driverlib.lib 
+
+C_SRCS += \
+../main_input.c \
+../tm4c1233h6pm_startup_ccs.c 
+
+C_DEPS += \
+./main_input.d \
+./tm4c1233h6pm_startup_ccs.d 
+
+OBJS += \
+./main_input.obj \
+./tm4c1233h6pm_startup_ccs.obj 
+
+OBJS__QUOTED += \
+"main_input.obj" \
+"tm4c1233h6pm_startup_ccs.obj" 
+
+C_DEPS__QUOTED += \
+"main_input.d" \
+"tm4c1233h6pm_startup_ccs.d" 
+
+C_SRCS__QUOTED += \
+"../main_input.c" \
+"../tm4c1233h6pm_startup_ccs.c" 
+
+
